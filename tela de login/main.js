8a76d4cc20680d5752a41b7415cd98c93a172674
LINUXh0flod <gabriel32bruninha@gmail.com>
2022-08-09 16:52:48 -0300

    Adicionando arquivos

---
+++ tela de login/main.js
@@ -0,0 +1,16 @@
+
+function entra() {
+var n = document.getElementById('iname')
+var s = document.getElementById('isenha')
+var t = document.getElementById('tab')
+if (n.value === 'back123779' && s.value === '34626563') {
+  
+  window.location.href = "home.html"
+
+} else {
+  
+  tab.innerHTML = 'Usuario ou Senha incorreta'
+}
+}
+
+
